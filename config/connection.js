ce1d5e89368ec0c3a7899b8d3f5328f12fbdb0d4
nikhilswain <nikhilswain36@gmail.com>
2020-07-26 14:52:20 +0530

    modified connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,4 @@
 module.exports = {
-    MongoURI:
-      'mongodb://niku_36:36_niku@registerex-shard-00-00.nsqzj.mongodb.net:27017,registerex-shard-00-01.nsqzj.mongodb.net:27017,registerex-shard-00-02.nsqzj.mongodb.net:27017/RegisterEx?ssl=true&replicaSet=atlas-fh79jh-shard-0&authSource=admin&retryWrites=true&w=majority',
-  };
-  
+  MongoURI:
+    'mongodb://<yourMongoDBusername>:<yourMongoDB_dbpassword>@registerex-shard-00-00.nsqzj.mongodb.net:27017,registerex-shard-00-01.nsqzj.mongodb.net:27017,registerex-shard-00-02.nsqzj.mongodb.net:27017/RegisterEx?ssl=true&replicaSet=atlas-fh79jh-shard-0&authSource=admin&retryWrites=true&w=majority',
+};
